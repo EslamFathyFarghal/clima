d548db32a793b839279584b61761ceab53126e8a
Eslam Fathy <eslmafathyfarghal@gmail.com>
2020-04-23 18:31:04 +0200

    delegate design pattern implemented , ui fully updating with the api response

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,24 +1,65 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
+ 
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+ class WeatherViewController: UIViewController, UITextFieldDelegate , WeatherManagerDelegate {
+    
+    
+  
+    
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    var weatherManager = WeatherHelper()
+    
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherManager.delegate = self 
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "You Should Enter a City !"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.getWeather(city: city)
+        }
+        searchTextField.text = ""
 
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+
+    }
+
+    func didUpdateWeather(weatherData: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weatherData.tempToString
+            self.conditionImageView.image = UIImage(systemName: weatherData.conditionName) 
+            self.cityLabel.text = weatherData.cityName
+        }
+        
+      }
+    func didFailWithAnError(error: Error) {
+        print("error")
+    }
 }
 
---
+++ WeatherHelper.swift
@@ -0,0 +1,66 @@
+//
+//  WeatherHelper.swift
+//  Clima
+//
+//  Created by Eslam Fathy on 4/23/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather (weatherData : WeatherModel)
+    func didFailWithAnError (error : Error)
+}
+
+class WeatherHelper {
+    
+    let apiKey = "3b44ee64617b9d737d9c958e174ab0fb"
+    let url = "https://api.openweathermap.org/data/2.5/weather?&units=metric"
+    var delegate : WeatherManagerDelegate?
+    
+    
+    func getWeather(city:String) {
+        let u =  "\(url)&appid=\(apiKey)&q=\(city)"
+        performRequest(url: u)
+        
+    }
+    
+    func performRequest(url :String) {
+        
+        if let u = URL(string: url) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: u) { (data, response, err) in
+                if err != nil {
+                    self.delegate?.didFailWithAnError(error: err!)
+                    return
+                }
+                if let responseData = data {
+                    if let weather = self.parseData(weatherData : responseData){
+                        self.delegate?.didUpdateWeather(weatherData: weather)
+                        
+                    }
+                }
+            }
+            task.resume()
+            
+        }
+    }
+    func parseData (weatherData : Data ) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather = WeatherModel(conditionId: id, temperature: temp, cityName: name)
+            return weather
+        } catch {
+            self.delegate?.didFailWithAnError(error: error)
+            return nil
+        }
+    }
+    
+    
+}
